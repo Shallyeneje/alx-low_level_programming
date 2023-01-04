088045f91b192447834b5d33a77450e1ef5dc41d
Shallyeneje <shalomeneje@gmail.com>
2023-01-04 10:07:46 -0800

    task-0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
  * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ * @s: pointer block of memory to fill
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
+		_putchar('\n');
+		return;
+	}
 		_putchar(*s);
 		_puts_recursions(s + 1);
-	}
 
-	else
-		_putchar('\n);
 }
 
